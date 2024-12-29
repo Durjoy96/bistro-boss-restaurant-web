6fc54218e4ca03fbd79116616de850ab4c408ba0
Durjoy96 <mddurjoy853@gmail.com>
2024-12-29 14:58:59 +0600

    navbar, banner, categories and footer complete

---
+++ src/pages/home/home/Home.jsx
@@ -0,0 +1,13 @@
+import Categories from "../../categories/Categories";
+import Banner from "../banner/Banner";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Categories />
+    </>
+  );
+};
+
+export default Home;
